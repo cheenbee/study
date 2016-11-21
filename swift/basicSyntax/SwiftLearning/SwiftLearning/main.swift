de9dd6cde8ba28ef2f5d58581f54ce73ea6a58ee
chenbing <cheenbee@163.com>
2016-11-21 19:00:24 +0800

    swift syntax study

---
+++ swift/basicSyntax/SwiftLearning/SwiftLearning/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SwiftLearning
+//
+//  Created by 艾呦呦 on 16/11/21.
+//  Copyright © 2016年 none. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let lexical =  LexicalStructure()
---
+++ swift/basicSyntax/SwiftLearning/SwiftLearning/syntax/LexicalStructure.swift
@@ -0,0 +1,19 @@
+//
+//  LexicalStructure.swift
+//  SwiftLearning
+//
+//  Created by 艾呦呦 on 16/11/21.
+//  Copyright © 2016年 none. All rights reserved.
+//  词法结构
+
+import Cocoa
+
+class LexicalStructure: NSObject {
+    override init() {
+        
+    }
+    
+//    func <#name#>(<#parameters#>) -> <#return type#> {
+//        <#function body#>
+//    }
+}
