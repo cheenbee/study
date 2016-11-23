e1d1b4bdec27cc7699a4c8d069828c76b2759d91
cheenbee <cheenbee@163.com>
2016-11-23 23:02:41 +0800

    string and Characters 2

--- swift/basicSyntax/SwiftLearning/SwiftLearning/syntax/Strings&Characters.swift
+++ swift/basicSyntax/SwiftLearning/SwiftLearning/syntax/Strings&Characters.swift
@@ -88,6 +88,46 @@ class Strings_Characters: NSObject {
         let range = (welcomeStr.index(welcomeStr.endIndex, offsetBy: -6, limitedBy: welcomeStr.startIndex) ?? welcomeStr.startIndex)..<welcomeStr.endIndex
         welcomeStr.removeSubrange(range)
         print("welcomeStr remove range \(range) then is : \(welcomeStr)")
+        
+        let str1 = "fuck"
+        let str2 = "FUCK"
+        if str1 == str2 {
+            print("\(str1) is equal \(str2)")
+        } else {
+            print("\(str1) is not equal \(str2)")
+        }
+        
+        let romeoAndJuliet = [
+            "Act 1 Scene 1: Verona, A public place",
+            "Act 1 Scene 2: Capulet's mansion",
+            "Act 1 Scene 3: A room in Capulet's mansion",
+            "Act 1 Scene 4: A street outside Capulet's mansion",
+            "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+            "Act 2 Scene 1: Outside Capulet's mansion",
+            "Act 2 Scene 2: Capulet's orchard",
+            "Act 2 Scene 3: Outside Friar Lawrence's cell",
+            "Act 2 Scene 4: A street in Verona",
+            "Act 2 Scene 5: Capulet's mansion",
+            "Act 2 Scene 6: Friar Lawrence's cell"
+        ]
+        var act1SceneCount = 0
+        for scene in romeoAndJuliet {
+            if scene.hasPrefix("Act 1") {
+                act1SceneCount += 1
+            }
+        }
+        print("There are \(act1SceneCount) scenes in act1")
+        
+        let dogString = "Dog‼🐶"
+        for codeUnit in dogString.utf8 {
+            print("\(codeUnit)", terminator: " ")
+        }
+        print("")
+        
+        for scalar in dogString.unicodeScalars {
+            print("\(scalar.value)", terminator:" ")
+        }
+        print("")
     }
     
 }
