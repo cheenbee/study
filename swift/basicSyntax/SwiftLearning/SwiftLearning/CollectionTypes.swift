e99e60e057f1ff2c436fb674fc29670cde010541
chenbing <cheenbee@163.com>
2016-11-29 19:00:52 +0800

    Array

--- swift/basicSyntax/SwiftLearning/SwiftLearning/CollectionTypes.swift
+++ swift/basicSyntax/SwiftLearning/SwiftLearning/CollectionTypes.swift
@@ -16,6 +16,26 @@ class CollectionTypes: NSObject {
     }
     
     func colloectionsDisplay() -> Void {
+        var someInts = [Int]()
+        someInts.append(3)
+        someInts.append(5)
+        someInts.insert(2, at: 0)
+        
+        for i in someInts {
+            print("\(i)", terminator: " ")
+        }
+        print("")
+        someInts = []
+        print("someInts has \(someInts.count) items")
+        
+        let threeDoubles = Array(repeating: 0.0, count: 3)
+        print("\(threeDoubles)")
+        
+        let anotherthreeDoubles = Array(repeating: 2.5, count: 5)
+        let sixDoubles = threeDoubles + anotherthreeDoubles
+        print("\(sixDoubles)")
+        
+        var shoppingList: [String] = ["Eggs", "Milk"]
         
     }
     
