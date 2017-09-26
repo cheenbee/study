91dfa586a6eb53e476f337585c2cadab0c28cd16
chenbing <chenbing@1992@gmail.com>
2017-09-26 18:53:47 +0800

    存储文档

---
+++ swift/basicSyntax/SwiftLearning/SwiftLearning/syntax/Closures.swift
@@ -0,0 +1,19 @@
+//
+//  Closures.swift
+//  SwiftLearning
+//
+//  Created by 艾呦呦 on 2017/5/22.
+//  Copyright © 2017年 none. All rights reserved.
+//
+
+import Cocoa
+
+class Closures: NSObject {
+    
+    let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+    func sortArray() -> Void {
+        
+    }
+    
+    
+}
